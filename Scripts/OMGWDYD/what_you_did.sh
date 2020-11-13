ca452bb30126e865f558f21f8f98531328d94401
Timothy Merritt <timmybytes@gmail.com>
2020-11-12 21:04:31 -0800

    (style): Change formatting for what_you_did output

--- Scripts/OMGWDYD/what_you_did.sh
+++ Scripts/OMGWDYD/what_you_did.sh
@@ -1,23 +1,26 @@
 #! /usr/bin/env bash
-# -------------------------------
+# ------------------------------------------------------
 # OH MY GOD, WHAT DID YOU DO?!
-# --------------------------------
-# title          :mkscript.sh
+# ------------------------------------------------------
+# title          :what_you_did.sh
 # description    :Checks previous day's git commits
 # author         :Timothy Merritt
 # date           :2020-11-12
 # version        :0.0.1
-# usage          :./runfile.sh
-# notes          :chmod +x runfile.sh to use this script
+# usage          :./what_you_did.sh
+# notes          :chmod +x runfile.sh to make executable
 # bash_version   :5.0.18(1)-release
+# ------------------------------------------------------
 
 # Clear screen
 clear
 
 # Header
-printf '=%.0s' {1..40}
+printf -- '-%.0s' {1..65}
 echo
-printf '%s\n' "LOOK AT WHAT YOU DID"
+printf '%s' "| LOOK AT WHAT YOU DID YESTERDAY"
+printf -- ' %.0s' {1..32}
+printf '%s\n' "|"
 
 # For each directory in #Repos
 for dir in /Users/nym/Projects/\#Repos/* ; do
@@ -27,15 +30,15 @@ for dir in /Users/nym/Projects/\#Repos/* ; do
   if [ -d .git ]; then
     # If repo has commits since yesterday, continue; else, next loop iteration
     if [[ $(git log --pretty=format:'%h was %an, %ar, message: %s' --since="yesterday") ]]; then
-      printf '=%.0s' {1..40}
+      printf -- '-%.0s' {1..65}
       echo
-      printf "Repository: "
+      printf "// Repository: "
       # Print name of current directory removed from full filepath
       printf '%s\n' "${PWD##*/}"
-      printf '=%.0s' {1..40}
+      printf -- '-%.0s' {1..65}
       echo
       # Print git log since yesterday, show only commits
-      git --no-pager log --since="yesterday" --pretty=tformat:"%x20%x20%s"
+      git --no-pager log --since="yesterday" --pretty=tformat:"%x20*%x20%x20%s"
     else
       continue
     fi
