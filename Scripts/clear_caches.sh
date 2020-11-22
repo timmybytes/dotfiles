d58e6332094f9124b1eab5c13940eab269db7f39
Timothy Merritt <timmybytes@gmail.com>
2020-11-22 06:59:22 -0800

    (refactor): Update cache script

--- Scripts/clear_caches.sh
+++ Scripts/clear_caches.sh
@@ -1,34 +1,52 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #===============================================================
 # CLEAR CACHES
 #===============================================================
 # Description: A simple script to clear out caches on Mac.
-# WARNING - This involves sudo, so be sure you're okay with deleting!
+# WARNING - Uses sudo — be sure you're okay with deleting!
 # Author: Timothy Merritt, 2020
 #===============================================================
 
-main () {
+d_usr() {
+  sudo rm -rf /Users/$USER/Library/Caches >>/Users/$USER/logs/cache_log.txt 2>&1
+  usr_status=$?
+  sleep 1
+  [ "$usr_status" -eq 0 ] && printf "%s\n" "User Cache successfully deleted" && sleep 1 && clear || printf "%s\n" "There were errors. Check ~/logs/cache_log for details"
+  # tree-list 1 level deep with human-readable filesizes
+  tree -shFL 1 --du /Users/$USER/Library/Caches
+}
+
+d_sys() {
+  sudo rm -rf /Library/Caches >>/Users/$USER/logs/cache_log.txt 2>&1
+  sys_status=$?
+  sleep 1
+  [ "$sys_status" -eq 0 ] && printf "%s\n" "System Cache successfully deleted" && sleep 1 && clear || printf "%s\n" "There were errors. Check ~/logs/cache_log for details"
+  # tree-list 1 level deep with human-readable filesizes
+  tree -shFL 1 --du /Library/Caches
+}
+
+main() {
   clear
   printf "%s" "// DELETE CACHES"
   printf "%s\n" "" "- To list current system caches, enter 'sys'"
   printf "%s" "" "- To list current user caches, enter 'usr'"
+  printf "%s" "" "- To delete both caches immediately, enter 'da'"
   printf "%s\n" "" "- Press 'Enter' to exit"
   printf "%s" "> "
   read input
-  if [ "$input" = "sys" ]; then
+  if [ "$input" = "da" ]; then
+    clear
+    d_usr
+    d_sys
+    main
+  elif [ "$input" = "sys" ]; then
     clear
     # tree-list 1 level deep with human-readable filesizes
     tree -shFL 1 --du /Library/Caches
     printf "%s\n" "Would you like to clear the sys cache? (y/n)"
     read sys_cache
     if [ "$sys_cache" = "y" ]; then
-      sudo rm -rf /Library/Caches
-      clear
-      sleep 1
-      printf "%s" "Sytem Cache successfully deleted"
-      sleep 1.5
-    # tree-list 1 level deep with human-readable filesizes
-      tree -shFL 1 --du /Library/Caches
+      d_sys
       main
     else
       main
@@ -40,20 +58,14 @@ main () {
     printf "%s\n" "Would you like to clear the usr cache? (y/n)"
     read usr_cache
     if [ "$usr_cache" = "y" ]; then
-      sudo rm -rf /Users/$USER/Library/Caches
-      clear
-      sleep 1
-      printf "%s" "User Cache successfully deleted"
-      sleep 1.5
-    # tree-list 1 level deep with human-readable filesizes
-      tree -shFL 1 --du /Users/$USER/Library/Caches
+      d_usr
       main
     else
       main
     fi
   else
     clear
-	cd
+    cd
     exit 0
   fi
 }
