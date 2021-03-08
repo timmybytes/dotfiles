18b8c5b8b86e8759a941c081b86fb23429d67750
Timothy Merritt <timmybytes@gmail.com>
2021-03-08 08:31:35 -0800

    feat(gitchechall): Add starter script for gitchecking local

---
+++ Scripts/gitcheckall.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#title          :gitcheckall.sh
+#description    :Check all local repo locations for status
+#author         :Timothy Merritt
+#date           :2021-03-08
+#version        :0.1.0  
+#usage          :./gitcheckall.sh
+#notes          :       
+#bash_version   :5.1.4(1)-release
+#============================================================================
+
+# Current WIP running only the most superficial of checks with mgitstatus
+clear
+echo "Checking repos..."
+cd ~
+mgitstatus -d 2 
+cd ~/Projects\/#Repos 
+mgitstatus -d 2
