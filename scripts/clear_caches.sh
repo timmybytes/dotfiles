c28a5fccf5779e3d559a05cc3ab1b51dc97835b5
Timothy Merritt <timmybytes@gmail.com>
2021-04-04 10:01:38 -0700

    chore(clear_caches): Improve formatting

--- scripts/clear_caches.sh
+++ scripts/clear_caches.sh
@@ -7,31 +7,44 @@
 # Author: Timothy Merritt, 2020
 #===============================================================
 
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color, resets
+PENDING="${YELLOW}●${NC}"
+FAILED="${RED}𐄂${NC}"
+SUCCEEDED="${GREEN}✓${NC}"
+
+clear
+
 d_usr() {
   sudo rm -rf /Users/$USER/Library/Caches >>/Users/$USER/logs/cache_log.txt 2>&1
   usr_status=$?
   sleep 1
-  [ "$usr_status" -eq 0 ] && printf "%s\n" "User Cache successfully deleted" && sleep 1 && clear || printf "%s\n" "There were errors. Check ~/logs/cache_log for details"
+  [ "$usr_status" -eq 0 ] && printf "%b\n" "${SUCCEEDED} User Cache successfully deleted" && sleep 1 || printf "%b\n" "${FAILED} There were errors. Check ~/logs/cache_log for details" && sleep 3
   # tree-list 1 level deep with human-readable filesizes
-  tree -shFL 1 --du /Users/$USER/Library/Caches
+  tree -aFL 1 -s -h -C --du /Users/$USER/Library/Caches
+  echo
 }
 
 d_sys() {
   sudo rm -rf /Library/Caches >>/Users/$USER/logs/cache_log.txt 2>&1
   sys_status=$?
   sleep 1
-  [ "$sys_status" -eq 0 ] && printf "%s\n" "System Cache successfully deleted" && sleep 1 && clear || printf "%s\n" "There were errors. Check ~/logs/cache_log for details"
+  [ "$sys_status" -eq 0 ] && printf "%b\n" "${SUCCEEDED} System Cache successfully deleted" && sleep 1 || printf "%s\b" "${FAILED} There were errors. Check ~/logs/cache_log for details" && sleep 3
   # tree-list 1 level deep with human-readable filesizes
-  tree -shFL 1 --du /Library/Caches
+  tree -aFL 1 -s -h -C --du /Library/Caches
+  echo
 }
 
 main() {
-  clear
   printf "%s\n" "// DELETE CACHES"
-  printf "%s" "" "- To list current system caches, enter 'sys'"
-  printf "%s\n" "" "- To list current user caches, enter 'usr'"
-  printf "%s" "" "- To delete both caches immediately, enter 'da'"
-  printf "%s\n" "" "- Press 'Enter' to exit"
+  printf "─%.0s" {1..46}
+  echo
+  printf "%s" "" "To list current system caches, enter 'sys'"
+  printf "%s\n" "" "To list current user caches, enter 'usr'"
+  printf "%s" "" "To delete both caches immediately, enter 'da'"
+  printf "%s\n" "" "Press 'Enter' to exit"
   printf "%s" "> "
   read input
   if [ "$input" = "da" ]; then
@@ -42,25 +55,29 @@ main() {
   elif [ "$input" = "sys" ]; then
     clear
     # tree-list 1 level deep with human-readable filesizes
-    tree -shFL 1 --du /Library/Caches
+    tree -aFL 1 -s -h -C --du /Library/Caches
     printf "%s\n" "Would you like to clear the sys cache? (y/n)"
     read sys_cache
     if [ "$sys_cache" = "y" ]; then
+      clear
       d_sys
       main
     else
+      clear
       main
     fi
   elif [ "$input" = "usr" ]; then
     clear
     # tree-list 1 level deep with human-readable filesizes
-    tree -shFL 1 --du /Users/$USER/Library/Caches
+    tree -aFL 1 -s -h -C --du /Users/$USER/Library/Caches
     printf "%s\n" "Would you like to clear the usr cache? (y/n)"
     read usr_cache
     if [ "$usr_cache" = "y" ]; then
+      clear
       d_usr
       main
     else
+      clear
       main
     fi
   else
