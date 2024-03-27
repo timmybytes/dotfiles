a96baeec9f57472fbe586676f6b1ab1c01d29f50
Timothy Merritt <timmybytes@gmail.com>
2024-03-27 07:10:42 -0700

    chore: Add default install script --WIP

---
+++ config/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#title          :install.sh
+#description    :Main install script for .dotfiles
+#author         :Timothy Merritt
+#date           :2024-03-27
+#version        :0.0.1
+#usage          :./install.sh
+#notes          :
+#bash_version   :5.2.26(1)-release
+#============================================================================
+
+#
