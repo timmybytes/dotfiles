89060fec8bd5269eb8145834ba9284fd3070a90d
Timothy Merritt <timmybytes@gmail.com>
2024-12-16 10:12:12 -0800

    chore(scripts): Add auto-git --WIP

---
+++ scripts/auto_git.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#title          :auto_git.sh
+#description    :Automated git repo commits and pushes
+#author         :Timothy Merritt
+#date           :2024-11-22
+#version        :0.1    
+#usage          :./auto_git.sh
+#notes          :       
+#bash_version   :5.1.8(1)-release
+#============================================================================
+
+
